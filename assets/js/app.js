6300906ed2d4ba7761e7eeb65e3a631dd58e8005
Amir <m00h4mm4dweb@gmail.com>
2024-11-05 07:53:17 -0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,13 +7,13 @@ window.addEventListener("load", function () {
     e.preventDefault();
   });
   
-  OnerrordefaultImage = "/assets/error-img.png";
+  OnerrordefaultImage = "https://raw.githubusercontent.com/6mir/q3/refs/heads/main/assets/error-img.png";
   function imageLoaded(img) {
     const loader = img.previousElementSibling;
     loader.style.display = "none";
     img.style.display = "block";
   }
   
-  JsonArtic = "/assets/data/singer.json";
-  JsonSong = "/assets/data/song.json";
-  JsonRemix = "/assets/data/remix.json";
+  JsonArtic = "https://raw.githubusercontent.com/6mir/q3/refs/heads/main/assets/data/singer.json";
+  JsonSong = "https://raw.githubusercontent.com/6mir/q3/refs/heads/main/assets/data/song.json";
+  JsonRemix = "https://raw.githubusercontent.com/6mir/q3/refs/heads/main/assets/data/remix.json";
